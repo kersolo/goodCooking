4e06c08b1f108047040c090ea7904372e9afda53
kersolo <loick.ferelloc@gmail.com>
2025-01-04 16:53:34 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@material-tailwind/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button>Button</button>
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "@material-tailwind/react";
+import "../material-tailwind.d.ts";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
